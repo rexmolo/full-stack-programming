66327aed101a070d3379da7c588feaeb8be195d9
rexmolo <jackie.hui.ma@gmail.com>
2023-11-21 11:21:46 +0100

    print entire week

--- 46week/_47week/src/main/java/io/github/rexmolo/Main.java
+++ 46week/_47week/src/main/java/io/github/rexmolo/Main.java
@@ -3,6 +3,6 @@ package io.github.rexmolo;
 public class Main {
     public static void main(String[] args) {
 
-        TimeTool.getCurrentDay();
+        TimeTool.printLastMondayAndLoopEntireWeek();
     }
 }
--- 46week/_47week/src/main/java/io/github/rexmolo/TimeTool.java
+++ 46week/_47week/src/main/java/io/github/rexmolo/TimeTool.java
@@ -1,9 +1,15 @@
 package io.github.rexmolo;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoField.DAY_OF_WEEK;
 
 public class TimeTool {
 
@@ -36,6 +42,21 @@ public class TimeTool {
     public static String getCurrentDaySpecialFormat(){
         LocalDateTime ldt = LocalDateTime.now();
         return ldt.format(DateTimeFormatter.ofLocalizedPattern("E d M"));
+    }
+
+    public static void printLastMondayAndLoopEntireWeek() {
+        LocalDate ld = LocalDate.now();
+        LocalDate lastMonday = ld.minusWeeks(1).with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+//        System.out.println(lastMonday);
+        System.out.println("Last Monday is: " + lastMonday);
+
+        System.out.println("The entire week is :");
+        for (int i = 0; i < 7; i++) {
+            LocalDate nextDay = lastMonday.plusDays(i);
+            String dayOfWeek = nextDay.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+            System.out.printf("%s is: %s%n", dayOfWeek, nextDay.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        }
+
     }
 
 }
