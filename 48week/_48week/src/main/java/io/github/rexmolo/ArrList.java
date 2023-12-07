968c0769c16c0bd4cb03b5ec180ea7c551a4d281
rexmolo <jackie.hui.ma@gmail.com>
2023-12-07 20:23:08 +0100

    array list object example

--- 48week/_48week/src/main/java/io/github/rexmolo/ArrList.java
+++ 48week/_48week/src/main/java/io/github/rexmolo/ArrList.java
@@ -1,16 +1,25 @@
 package io.github.rexmolo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+record Person(String firstName, String lastName, int id) {
+    public Person(String firstName, String lastName) {
+
+        this(firstName, lastName, 1);
+    }
+}
 
 public class ArrList {
 
     public static void main(String[] args) {
 
-        listExample();
+        listObjectExample();
     }
 
-    public static void listExample() {
+    public static void listStringExample() {
         String[] books = {"one", "two", "three"};
         List booksList = List.of(books);
         // can not call booksList.add();
@@ -21,15 +30,39 @@ public class ArrList {
         ArrayList<String> nextBookArrList = new ArrayList<>(
                 List.of("five", "six" , "seven")
         );
+
+        bookArrList.addAll(Arrays.asList("eggs", "cooking"));
+
         bookArrList.addAll(nextBookArrList);
 
-        bookArrList.removeAll(List.of("three", "seven"));
-        bookArrList.retainAll(List.of("four", "one"));
+        bookArrList.sort(Comparator.naturalOrder());
+        bookArrList.sort(Comparator.reverseOrder());
 
-        bookArrList.clear();
+        //change to array
+        String[] bookArr = bookArrList.toArray(new String[bookArrList.size()]);
 
-        bookArrList.isEmpty();
+//        bookArrList.removeAll(List.of("three", "seven"));
+//        bookArrList.retainAll(List.of("four", "one"));
 
-        System.out.println(bookArrList);
+//        bookArrList.clear();
+
+//        bookArrList.isEmpty();
+
+//        System.out.println(bookArrList);
+        System.out.println(Arrays.toString(bookArr));
+    }
+
+    public static void listObjectExample() {
+        Person[] pl = new Person[]{};
+
+        ArrayList<Person> pall = new ArrayList<>(List.of(pl));
+        pall.add(new Person("jackie", "Ma"));
+        pall.add(new Person("Goo", "T"));
+
+        pl = pall.toArray(new Person[pall.size()]);
+
+//        System.out.println(pall);
+        System.out.println(Arrays.toString(pl));
+
     }
 }
