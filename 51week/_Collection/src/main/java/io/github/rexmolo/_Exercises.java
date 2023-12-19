aaffa33e5e5b6e2cfb3499e3f1ca5c2c6216e177
rexmolo <jackie.hui.ma@gmail.com>
2023-12-19 18:59:51 +0100

    [collections-excerises]

---
+++ 51week/_Collection/src/main/java/io/github/rexmolo/_Exercises.java
@@ -0,0 +1,16 @@
+package io.github.rexmolo;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _Exercises {
+
+    public static void listWeek() {
+        List<String> weekdays = new ArrayList<>();
+        System.out.println();//DayOfWeek.MONDAY
+    }
+}
