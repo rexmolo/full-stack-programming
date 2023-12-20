b7f3db0e4ebd0c4ae2880212c408c863b5ca45ce
rexmolo <jackie.hui.ma@gmail.com>
2023-12-20 21:56:32 +0100

    Course and Student

--- project/workshop-course-manager/src/main/java/se/lexicon/course_manager/model/Course.java
+++ project/workshop-course-manager/src/main/java/se/lexicon/course_manager/model/Course.java
@@ -1,5 +1,75 @@
 package se.lexicon.course_manager.model;
 
-public class Course {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
 
+public class Course implements Serializable {
+
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private Collection<Student> students;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        Course course = (Course) o;
+        return id == course.id && weekDuration == course.weekDuration && Objects.equals(courseName, course.courseName) && Objects.equals(startDate, course.startDate) && Objects.equals(students, course.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
 }
--- project/workshop-course-manager/src/main/java/se/lexicon/course_manager/model/Student.java
+++ project/workshop-course-manager/src/main/java/se/lexicon/course_manager/model/Student.java
@@ -1,5 +1,64 @@
 package se.lexicon.course_manager.model;
 
-public class Student {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Student implements Serializable {
+
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(address, student.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
