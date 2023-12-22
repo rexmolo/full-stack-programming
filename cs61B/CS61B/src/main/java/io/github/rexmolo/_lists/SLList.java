2fce4f03407ae5b74cd9bc1bce4b57731f73d9a5
rexmolo <jackie.hui.ma@gmail.com>
2023-12-22 22:49:19 +0100

    simple list

---
+++ cs61B/CS61B/src/main/java/io/github/rexmolo/_lists/SLList.java
@@ -0,0 +1,39 @@
+package io.github.rexmolo._lists;
+
+public class SLList<T> {
+
+    private class StuffNode {
+        public T item;
+        public StuffNode next;
+
+        public StuffNode(T i, StuffNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
+    public StuffNode first;
+    public int size;
+
+    public SLList(T x) {
+        first = new StuffNode(x, null);
+        size = 1;
+    }
+
+    public void addFirst(T x) {
+        first = new StuffNode(x, first);
+        size += 1;
+    }
+
+    public T getFirst(){
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        SLList<String> sl = new SLList<String>("one");
+        sl.addFirst("two");
+        System.out.println(sl.getFirst());
+    }
+
+
+}
