975951bb968dfb4793bcec7ff3120b5b1ca7a2b9
rexmolo <jackie.hui.ma@gmail.com>
2024-01-19 16:07:37 +0100

    inject properties

--- self_practices/app-1/src/main/java/io/github/rexmolo/app1/Rest/FunRestController.java
+++ self_practices/app-1/src/main/java/io/github/rexmolo/app1/Rest/FunRestController.java
@@ -1,10 +1,14 @@
 package io.github.rexmolo.app1.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("$(coach.name)")
+    private String coachName;
 
     @GetMapping("/")
     public String sayHello() {
