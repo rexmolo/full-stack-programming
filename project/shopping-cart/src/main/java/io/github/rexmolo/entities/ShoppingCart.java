d4018b8a6172e4dcf40634a1a9142cbb3e72529f
rexmolo <jackie.hui.ma@gmail.com>
2024-01-24 10:27:35 +0100

    init and shopping cart entities

---
+++ project/shopping-cart/src/main/java/io/github/rexmolo/entities/ShoppingCart.java
@@ -0,0 +1,77 @@
+package io.github.rexmolo.entities;
+
+import java.time.LocalDateTime;
+
+public class ShoppingCart {
+    private int id;
+    private LocalDateTime lastUpdate;
+    private String orderStatus;
+    private String deliveryAddress;
+    private String customerReference;
+    private boolean paymentApproved;
+
+    public ShoppingCart(int id, LocalDateTime lastUpdate, String orderStatus, String deliveryAddress, String customerReference, boolean paymentApproved) {
+        this.id = id;
+        this.lastUpdate = lastUpdate;
+        this.orderStatus = orderStatus;
+        this.deliveryAddress = deliveryAddress;
+        this.customerReference = customerReference;
+        this.paymentApproved = paymentApproved;
+    }
+
+    public ShoppingCart(LocalDateTime lastUpdate, String orderStatus, String deliveryAddress, String customerReference, boolean paymentApproved) {
+        this.lastUpdate = lastUpdate;
+        this.orderStatus = orderStatus;
+        this.deliveryAddress = deliveryAddress;
+        this.customerReference = customerReference;
+        this.paymentApproved = paymentApproved;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getCustomerReference() {
+        return customerReference;
+    }
+
+    public void setCustomerReference(String customerReference) {
+        this.customerReference = customerReference;
+    }
+
+    public boolean isPaymentApproved() {
+        return paymentApproved;
+    }
+
+    public void setPaymentApproved(boolean paymentApproved) {
+        this.paymentApproved = paymentApproved;
+    }
+}
